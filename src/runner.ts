3ad43a34c8379a274b06d0de10e03bb139c2d9da
Brian Watkins <brian.watkins@gmail.com>
2023-10-29 08:42:25 -0400

    Terminate on uncaught exception

--- src/runner.ts
+++ src/runner.ts
@@ -1,9 +1,9 @@
 import { getBehaviorsMatchingPattern } from "./behaviorCollector.js"
 import { SequentialValidator } from "./sequentialValidator.js"
-import { OrderProvider, Reporter } from "esbehavior"
+import { OrderProvider, Reporter, Summary } from "esbehavior"
 import { PlaywrightBrowser } from "./playwrightBrowser.js"
 import { Transpiler } from "./transpiler.js"
-import { BehaviorEnvironment } from "./behaviorMetadata.js"
+import { BehaviorEnvironment, BehaviorMetadata } from "./behaviorMetadata.js"
 import { BehaviorFactory } from "./behaviorFactory.js"
 import { BrowserBehaviorContext } from "./browserBehavior.js"
 import { DocumentationRunner } from "./documentationRunner.js"
@@ -25,19 +25,29 @@ export class Runner {
   }
 
   async run(options: RunOptions): Promise<void> {
-    const behaviors = await getBehaviorsMatchingPattern({
+    try {
+      const behaviors = await this.getBehaviors(options)
+      const summary = await this.validateBehaviors(behaviors, options)
+      if (summary.invalid > 0 || summary.skipped > 0) {
+        process.exitCode = 1
+      }
+    } catch (err: any) {
+      options.reporter.terminate(err)
+      process.exitCode = 1
+    }
+  }
+
+  private async getBehaviors(options: RunOptions): Promise<Array<BehaviorMetadata>> {
+    return await getBehaviorsMatchingPattern({
       pattern: options.behaviorPathPattern,
       defaultEnvironment: options.defaultEnvironment
     })
+  }
 
+  private async validateBehaviors(behaviors: Array<BehaviorMetadata>, options: RunOptions): Promise<Summary> {
     const runner = new DocumentationRunner(options)
-
-    // need to call terminate if this throws an exception
     const validator = new SequentialValidator(this.behaviorFactory, runner)
-    const summary = await validator.validate(behaviors, options)
 
-    if (summary.invalid > 0 || summary.skipped > 0) {
-      process.exitCode = 1
-    }
+    return await validator.validate(behaviors, options)
   }
 }
